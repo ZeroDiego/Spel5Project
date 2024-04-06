565ab250425ca1c31098eaa5c39d90ffbc7a4e63
ZeroDiego <82512083+ZeroDiego@users.noreply.github.com>
2024-04-06 20:58:27 +0200

    (Unfinished) DogAI part 1

---
+++ Source/Spel5Project/DogAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "DogAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPEL5PROJECT_API ADogAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
