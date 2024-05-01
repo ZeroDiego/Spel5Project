6e66b653832e04fcf1fa503866aee4f091807f71
ErikStrandberg1 <113352065+ErikStrandberg1@users.noreply.github.com>
2024-05-01 17:13:56 +0200

    update from main into eik
    sdaf

--- Source/Spel5Project/PanActor.h
+++ Source/Spel5Project/PanActor.h
@@ -26,6 +26,9 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	class UBoxComponent * CollisionBox;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool isPoisoned;
+
 	UFUNCTION()
 	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
@@ -35,5 +38,4 @@ public:
 	FORCEINLINE bool GetIsOverlapping() const { return bIsOverlapping; }
 	
 	bool bIsOverlapping;
-	bool isPoisoned;
 };
