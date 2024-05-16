b06e43a93e7ee6a5cb891f11b4def7bb70a502f4
Debigcheese <lucas_story01@yahoo.com>
2024-05-16 17:42:15 +0200

    Reapply "Merge branch 'main' into Berg3"
    This reverts commit 78df1ac44cf528d6553f7fcb4e5238fe11df49c1.

--- Source/Spel5Project/DogCharacter.h
+++ Source/Spel5Project/DogCharacter.h
@@ -10,65 +10,65 @@
 UCLASS()
 class SPEL5PROJECT_API ADogCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	ADogCharacter();
+    // Sets default values for this character's properties
+    ADogCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
 
 public:
-	bool GetIsSatisfied() const;
-	
-	void SetSatisfied(bool NewIsSatisfied);
-	
-	bool GetIsSleeping() const;
-	
-	void SetIsSleeping(bool NewIsSleeping);
+    bool GetIsSatisfied() const;
 
-	bool GetHasEaten() const;
-	
-	void SetHasEaten(bool NewHasEaten);
+    void SetSatisfied(bool NewIsSatisfied);
 
-	void Alert();
-	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    bool GetIsSleeping() const;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    void SetIsSleeping(bool NewIsSleeping);
 
-	UMover* GetMover() const;
-	
-	UFUNCTION(BlueprintCallable)
-	void SetMover(UMover* NewMover);
-	
+    bool GetHasEaten() const;
+
+    void SetHasEaten(bool NewHasEaten);
+
+    void Alert();
+
+    // Called every frame
+    virtual void Tick(float DeltaTime) override;
+
+    // Called to bind functionality to input
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+    UMover* GetMover() const;
+
+    UFUNCTION(BlueprintCallable)
+    void SetMover(UMover* NewMover);
+
 private:
-	UPROPERTY(VisibleAnywhere)
-	bool IsSatisfied;
+    UPROPERTY(VisibleAnywhere)
+    bool IsSatisfied;
 
-	UPROPERTY(VisibleAnywhere)
-	bool IsSleeping;
-	
-	UPROPERTY(VisibleAnywhere)
-	bool HasEaten;
+    UPROPERTY(VisibleAnywhere)
+    bool IsSleeping;
 
-	UPROPERTY(VisibleAnywhere)
-	bool IsAlert;
-	
-	UPROPERTY(EditAnywhere)
-	float SleepDelay = 1;
-	
-	UPROPERTY(EditAnywhere)
-	USoundBase* BarkSound;
+    UPROPERTY(VisibleAnywhere)
+    bool HasEaten;
 
-	UPROPERTY(EditAnywhere)
-	USoundBase* EatSound;
-	
-	FTimerHandle SleepTimer;
+    UPROPERTY(VisibleAnywhere)
+    bool IsAlert;
 
-	UMover* Mover;
+    UPROPERTY(EditAnywhere)
+    float SleepDelay = 1;
+
+    UPROPERTY(EditAnywhere)
+    USoundBase* BarkSound;
+
+    UPROPERTY(EditAnywhere)
+    USoundBase* EatSound;
+
+    FTimerHandle SleepTimer;
+
+    UMover* Mover;
 };
