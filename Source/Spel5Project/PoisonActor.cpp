2e9e3cbd47923300d0cae2ba87fb875d9dd1cd90
lucasoden <lucasoden1@hotmail.com>
2024-05-20 12:44:30 +0200

    Tog bort lite skräp

--- Source/Spel5Project/PoisonActor.cpp
+++ Source/Spel5Project/PoisonActor.cpp
@@ -4,10 +4,9 @@
 #include "PoisonActor.h"
 #include "Components/BoxComponent.h"
 
-// Sets default values
 APoisonActor::APoisonActor()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+
 	PrimaryActorTick.bCanEverTick = true;
 
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
@@ -17,14 +16,12 @@ APoisonActor::APoisonActor()
 
 }
 
-// Called when the game starts or when spawned
 void APoisonActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
-// Called every frame
 void APoisonActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
