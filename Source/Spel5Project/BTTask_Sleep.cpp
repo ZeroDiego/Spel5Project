6b07bdd2d968191630931cf81def8ad17ffb0558
lucasoden <113351500+lucasoden@users.noreply.github.com>
2024-05-22 14:18:01 +0200

    Merge fix AI

--- Source/Spel5Project/BTTask_Sleep.cpp
+++ Source/Spel5Project/BTTask_Sleep.cpp
@@ -1,17 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "BTTask_OpenDogWall.h"
+#include "BTTask_Sleep.h"
 #include "AIController.h"
 #include "DogCharacter.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
-UBTTask_OpenDogWall::UBTTask_OpenDogWall()
+UBTTask_Sleep::UBTTask_Sleep()
 {
-	NodeName = TEXT("Open Wall");
+	NodeName = TEXT("Sleep");
 }
 
-EBTNodeResult::Type UBTTask_OpenDogWall::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+EBTNodeResult::Type UBTTask_Sleep::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
 	Super::ExecuteTask(OwnerComp, NodeMemory);
 
@@ -26,12 +25,6 @@ EBTNodeResult::Type UBTTask_OpenDogWall::ExecuteTask(UBehaviorTreeComponent& Own
 		return EBTNodeResult::Failed;
 	}
 
-	UMover* Mover = DogCharacter->GetMover();
-	if (Mover == nullptr)
-	{
-		return EBTNodeResult::Failed;
-	}
-
-	Mover->SetShouldMove(true);
+	DogCharacter->SetIsSleeping(true);
 	return EBTNodeResult::Succeeded;
 }
